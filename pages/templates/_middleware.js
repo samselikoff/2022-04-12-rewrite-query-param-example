58434f39cc267a7bbfbbc7b537ec9be7e085c1aa
Sam Selikoff <sam.selikoff@gmail.com>
2022-04-12 14:43:02 -0400

    wip

--- pages/templates/_middleware.js
+++ pages/templates/_middleware.js
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
 
-let allowedParams = ["allowed"];
-
 export function middleware(req) {
   let url = req.nextUrl.clone();
-  // let changed = false;
 
   if (url.search === "") {
     url.pathname = `${url.pathname}/prerendered`;
@@ -13,18 +10,4 @@ export function middleware(req) {
   }
 
   return NextResponse.rewrite(url);
-  // url.searchParams.forEach((_, key) => {
-  //   if (!allowedParams.includes(key)) {
-  //     url.searchParams.delete(key);
-  //     changed = true;
-  //   }
-  // });
-
-  // Avoid infinite loop by only redirecting if the query
-  // params were changed
-  // if (changed) {
-  //   return NextResponse.redirect(url);
-  //   // It's also useful to do a rewrite instead of a redirect
-  //   // return NextResponse.rewrite(url)
-  // }
 }
